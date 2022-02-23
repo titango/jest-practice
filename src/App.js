7292242798a68cec076b61e42f736a0920ba3b5b
Tan Thinh Ngo <ngotanthinh92@gmail.com>
2022-02-22 23:21:39 -0800

    Write new test for checkbox clicked and disabled button

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [buttonColor, setButtonColor] = useState('red');
+  const [ buttonColor, setButtonColor ] = useState('red');
+  const [ disabled, setdisabled ] = useState(false);
+  
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
-
+  
   return (
-    <div >
-      <button 
-        style={{ 
-          color: 'white',
-          backgroundColor: buttonColor
-        }}
+    <div>
+        <button
+        style={{backgroundColor: buttonColor, color: 'white'}}
         onClick={() => setButtonColor(newButtonColor)}
+        disabled={disabled}
       >Change to {newButtonColor}</button>
-      <input type="checkbox" />
-    </div>
+    <br />
+    <input
+        type="checkbox"
+        id="enable-button-checkbox"
+        defaultChecked={disabled}
+        aria-checked={disabled}
+        onChange={(e) => setdisabled(e.target.checked)} />
+    </div>  
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -31,3 +31,28 @@ test('initial conditions', () => {
   const checkbox = screen.getByRole('checkbox');
   expect(checkbox).not.toBeChecked();
 });
+
+test('Checkbox checked and disabled button', () => {
+  render(<App />);
+
+  // check that the button starts out enabled
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  expect(colorButton).toBeEnabled();
+
+  // check that the checkbox starts out unchecked
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeChecked();
+
+  // Click checkbox
+  fireEvent.click(checkbox);
+  
+  // Check checkbox is checked and button is disabled
+  expect(checkbox).toBeChecked();
+  expect(colorButton).toBeDisabled();
+  
+  // Reopen state
+  fireEvent.click(checkbox);
+  expect(checkbox).not.toBeChecked();
+  expect(colorButton).toBeEnabled();
+
+});
