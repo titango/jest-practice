7996c492d06c52478e7e22d925082c4e9a31bbd4
Tan Thinh Ngo <ngotanthinh92@gmail.com>
2022-03-13 01:06:33 -0800

    Add Alert and test for error response

--- sundae-frontend/src/pages/entry/Options.jsx
+++ sundae-frontend/src/pages/entry/Options.jsx
@@ -3,9 +3,11 @@ import { useEffect, useState } from 'react';
 import ScoopOption from './ScoopOption';
 import Row from 'react-bootstrap/row';
 import ToppingOption from './ToppingOption';
+import AlertBanner from '../common/AlertBanner';
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
   // optionType is 'scoops' or 'toppings'
 
   useEffect(() => {
@@ -13,9 +15,13 @@ export default function Options({ optionType }) {
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
       .catch((error) => {
-        // TODO: Handle error
+        setError(true);
       });
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   // TODO: replace `null` with ToppingOption when available
   const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
